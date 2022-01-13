7d7c5c0e8a2c11347284cd0e1f9249ca24d4ec91
neetagupta23 <96100132+neetagupta23@users.noreply.github.com>
2022-01-13 10:26:26 +0530

    Update set_env.sh

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=udacitypostgre
-export POSTGRES_HOST=postgres.c772n0goeqgt.us-east-2.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::udagram-bucket-ng
-export AWS_REGION=us-east-2
-export AWS_PROFILE=default
-export JWT_SECRET=hello
+export POSTGRES_PASSWORD=password1
+export POSTGRES_HOST=udagram2.csixzufepzrt.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=udagram
+export AWS_BUCKET=arn:aws:s3:::udaconnect
+export AWS_REGION=us-west-2
+export AWS_PROFILE=test
+export JWT_SECRET=testing
 export URL=http://localhost:8100
+
